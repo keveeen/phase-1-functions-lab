942e1f9efa32c6c8f792e743992a2f39465dadc0
kevin breid <kevinbreid@yahoo.com>
2022-06-09 17:07:38 -0700

    commit tree 882e181f4201ed5120f8f7f3e0f346ca30f9ceef

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-// Code your solution in this file!
+
+const headquarters = 42
+
+function distanceFromHqInBlocks(pickup){
+
+    if (pickup >= 42){
+        return pickup - headquarters
+    }
+
+    else {
+        return headquarters - pickup
+    }
+
+}
+
+
+function distanceFromHqInFeet(pickup){
+
+    return distanceFromHqInBlocks(pickup) * 264
+}
+
+//function distanceTravelledInFeet(start, destination){
+
+const distanceTravelledInFeet = function(start, destination){
+
+    if (start >= destination){
+        return (start - destination) * 264
+    }
+    else {
+        return (destination -  start) * 264
+    }
+}
+
+function calculatesFarePrice(start, destination){
+
+    if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000){
+
+        return .02 * (distanceTravelledInFeet(start, destination) - 400)
+    }
+
+    else if (distanceTravelledInFeet(start, destination) >= 2000 && distanceTravelledInFeet(start, destination) < 2500){
+            return 25
+    }
+
+    else if (distanceTravelledInFeet(start, destination) < 400){
+        return 0
+    }
+
+    else if (distanceTravelledInFeet(start, destination) > 2500){
+        return 'cannot travel that far'
+    }
+
+    
+}
+
